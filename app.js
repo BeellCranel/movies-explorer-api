bc890a64c2afcd3e2ddd69b0de0615cdea5f8bbd
Dergach Roman <dergachroma@gmail.com>
2022-07-02 12:45:43 +0300

    feat: настроил сервер, подключил бд

---
+++ app.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// здесь будут руты
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
