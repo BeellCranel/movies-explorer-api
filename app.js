bc7bfd7e7e47ee39ee92ef971665ddbe582217ca
Dergach Roman <dergachroma@gmail.com>
2022-07-04 17:31:22 +0300

    feat: добавил корс и подредактировал ReadMe

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+
+const allowedCors = [
+  'http://localhost:3000',
+  'http://my-website.nomoredomains.xyz',
+  'https://my-website.nomoredomains.xyz',
+];
 
 const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 
+app.use(cors({
+  origin: allowedCors,
+}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
