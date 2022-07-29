9255fe62a19ae3790adaf4a868488e7c236eba48
Dergach Roman <dergachroma@gmail.com>
2022-07-29 16:12:28 +0300

    feat: повысил количество запросов в минуту

--- middlewares/rateLimit.js
+++ middlewares/rateLimit.js
@@ -2,12 +2,12 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 1000,
-  max: 2,
+  max: 10,
 });
 
 const createUserLimiter = rateLimit({
   wondowMs: 60 * 60 * 1000,
-  max: 6,
+  max: 10,
   message: 'С вышего IP было создано слишком много аккаунтов, пожалуйста попробуйте снова через час',
 });
 
